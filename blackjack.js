2b03ed444b62493652b3cbb618a89dd3915d0386
22jdillon <22jdillon@wakatipu.school.nz>
2025-07-29 18:07:28 +1200

    Javascript Building Deck

---
+++ blackjack.js
@@ -0,0 +1,27 @@
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var yourAceCount = 0;
+
+var hidden;
+var deck;
+
+var canHit = true; //allows the plater to draw while yourSum <= 21
+
+window.onload = function() {
+    buildDeck();
+}
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
+        }
+    }
+    console.log(deck);
+}
