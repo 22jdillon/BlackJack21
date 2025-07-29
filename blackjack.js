fd357fdcd4e99d24ab7b0f6214b78de1cfc496ff
22jdillon <22jdillon@wakatipu.school.nz>
2025-07-29 18:28:43 +1200

    Shuffling Deck

--- blackjack.js
+++ blackjack.js
@@ -11,6 +11,7 @@ var canHit = true; //allows the plater to draw while yourSum <= 21
 
 window.onload = function() {
     buildDeck();
+    shuffleDuck();
 }
 
 function buildDeck() {
@@ -22,6 +23,16 @@ function buildDeck() {
         for (let j = 0; j < values.length; j++) {
             deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
         }
+    }
+    // console.log(deck);
+}
+
+function shuffleDuck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length); //Math.random is going to give a number between 0 and 1 and then it will multiply that by the length of the deck which gives us a number which gives us a floating number between 0-51.999... so we will use math.floor to get an intiger. 
+        let temp = deck[i];
+        deck[i] = deck[j]
+        deck[j] = temp;
     }
     console.log(deck);
 }
